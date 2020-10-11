1f484bda3480e55c5094f9284dc372f0566be1c3
FredDlt <fredy.delatorre.frd@gmail.com>
2020-10-10 22:52:42 -0500

    Lista Tipo Agenda

--- app/src/main/java/com/example/semana05/MainActivity.kt
+++ app/src/main/java/com/example/semana05/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.semana05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.semana05.recyclerView.Persona
+import com.example.semana05.recyclerView.PersonaAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lista = ArrayList<Persona>()
+        lista.add(Persona(nombre = "asdffsadf", numero = "3453245234"))
+        lista.add(Persona(nombre = "asdf3f", numero = "34545234"))
+        lista.add(Persona(nombre = "assadf", numero = "3453234"))
+        lista.add(Persona(nombre = "asdf", numero = "3453234"))
+        rvAgenda.layoutManager = LinearLayoutManager(this)
+        rvAgenda.adapter = PersonaAdapter(lista)
     }
 }
--- app/src/main/java/com/example/semana05/recyclerView/PersonaAdapter.kt
+++ app/src/main/java/com/example/semana05/recyclerView/PersonaAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.semana05.recyclerView
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.semana05.R
 
-class PersonaAdapter : RecyclerView.Adapter<PersonaVH>(){
+class PersonaAdapter (val listaPersonas: ArrayList<Persona>): RecyclerView.Adapter<PersonaVH>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonaVH {
-
+        val vista = LayoutInflater.from(parent.context).inflate(R.layout.item_persona, parent, false)
+        return PersonaVH(vista)
     }
 
     override fun onBindViewHolder(holder: PersonaVH, position: Int) {
-
+        val person = listaPersonas[position]
+        holder.lblNombre.text = person.nombre
+        holder.lblNumero.text = person.numero
     }
 
     override fun getItemCount(): Int {
-
+        return listaPersonas.size
     }
 
 }
